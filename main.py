cb44e1286373d052844076765df628c0bcdbf601
haneul-yoo <skysky2387@gmail.com>
2021-11-02 21:24:54 +0900

    language task selection

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from ast import literal_eval
 app = Flask(__name__)
 data_path = './data'
 output_path = './output'
-context_count_per_user = 10
+context_count_per_user = 3
 user_count_per_context = 3
 secret_code = 'done_'
 
@@ -25,12 +25,6 @@ def init_paths():
     for path in paths:
         if not os.path.exists(path):
             os.makedirs(path)
-
-
-@app.route('/')
-def index():
-    return redirect('/tasks')
-
 
 def get_all_context_ids():
     context_filenames = os.listdir('%s/contexts' % data_path)
@@ -97,8 +91,14 @@ def get_validate_texts():
         validate_texts = json.load(f)
     return validate_texts
 
+def draw_question_ids_over_limit():
+    response_filenames = os.listdir('%s/response' % output_path)
+    response_ids = [x.split('__res__')[0].split('/')[-1] for x in response_filenames]
+    response_counter = Counter(response_ids)
+    return [k for k, v in response_counter.items() if v >= user_count_per_context]
 
-def draw_context_dicts():
+
+def draw_context_dicts(language_task_set):
 
     json_url = requests.get('https://sheets.googleapis.com/v4/spreadsheets/1DPQnBmAQtJ0pCYGgD7dSmoN8EUKWU10eGUjPJ76B5TE/values/dataset/?alt=json&key=AIzaSyAQRP6ZxaLICxsOCQowChrdDfghUASYzcs').json()['values']
 
@@ -106,6 +106,8 @@ def draw_context_dicts():
     
     questions = json_url[1:]
     random.shuffle(questions)
+    questions_over_limit = draw_question_ids_over_limit()
+    questions = [q for q in questions if q[3] not in questions_over_limit and [q[2], q[0]] in language_task_set]
     questions = questions[:min(context_count_per_user, len(questions))]
 
     questions = [dict(zip(header, v)) for v in questions]
@@ -132,32 +134,64 @@ def generate_user_id():
     return uid
 
 
-def save_response(res_output_path, context_id, user_id, response, isPassed):
+def save_response(res_output_path, context_id, user_id, response, isPassed, workerId, start_time, end_time):
     if isPassed:
-        file_path = '%s/%s__res__%s.json' % (res_output_path, context_id, user_id)
+        file_path = '%s/%s__res__%s__%s.json' % (res_output_path, context_id, user_id, workerId)
     else:
-        file_path = '%s/no_pass__%s__res__%s.json' % (res_output_path, context_id, user_id)
+        file_path = '%s/no_pass__%s__res__%s__%s.json' % (res_output_path, context_id, user_id, workerId)
+    data = {
+        'context_id': context_id,
+        'user_id': user_id,
+        'response': response,
+        'worker_id': workerId,
+        'start_time': start_time,
+        'end_time': end_time
+    }
     with open(file_path, 'w') as f:
-        f.write(json.dumps(response))
+        # f.write(json.dumps(response))
+        f.write(json.dumps(data, ensure_ascii=False))
 
+
+def get_language_task_set():
+    json_url = requests.get('https://sheets.googleapis.com/v4/spreadsheets/1DPQnBmAQtJ0pCYGgD7dSmoN8EUKWU10eGUjPJ76B5TE/values/dataset/?alt=json&key=AIzaSyAQRP6ZxaLICxsOCQowChrdDfghUASYzcs').json()['values']
+    language_task_set = list(set((row[2], row[0]) for row in json_url[1:]))
+    # language_task_set = list({v['language']:v for v in [{'language': row[2], 'task': row[0]} for row in json_url[1:]]}.values())
+    return language_task_set
+
+
+@app.route('/')
+def index():
+    return redirect('/tasks')
 
 @app.route('/tasks')
 def task_index():
     return render_template('task_index.html')
 
+@app.route('/tasks/instruction')
+def task_instruction():
+    language_task_set = get_language_task_set()
+    return render_template('task_instruction.html',
+        language_task_set=language_task_set)
 
-@app.route('/tasks/draw')
+@app.route('/tasks/draw', methods=['POST'])
 def task_draw():
+    # print('request.data:', request.data)
+    data = json.loads(request.data)
+    print(data)
+    workerId = data['workerId']
+    language_task_set = data['language_task_set']
     uid = generate_user_id()
-    context_dicts = draw_context_dicts()
+    context_dicts = draw_context_dicts(language_task_set)
     questions = get_questions()
     validate_texts = get_validate_texts()
-    return render_template(
-        'task_draw.html',
+    redirect('/tasks') if workerId and language_task_set else ''
+    print('context_dicts:', context_dicts)
+    return render_template('task_draw.html',
         uid=uid,
         contexts=context_dicts,
         questions=questions,
-        validate_texts=validate_texts)
+        validate_texts=validate_texts,
+        workerId=workerId)
 
 
 @app.route('/tasks/submit', methods=['POST'])
@@ -166,6 +200,9 @@ def task_submit():
     response = data['response']
     user_id = data['uid']
     isPassed = data['isPassed']
+    workerId = data['workerId']
+    start_time = data['start_time']
+    end_time = data['end_time']
     # validatorValues = data['validatorValues']
 
     if isPassed:
@@ -175,7 +212,7 @@ def task_submit():
         save_response(res_output_path, "attention", user_id, validatorValues, isPassed)
 
     for context_id, value in response.items():
-        save_response(res_output_path, context_id, user_id, value, isPassed)
+        save_response(res_output_path, context_id, user_id, value, isPassed, workerId, start_time, end_time)
 
     return 'done:%s' % (secret_code + user_id)
 
